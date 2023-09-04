94cb3108f3c70b2a7300ac7eea46447a119d4198
Ivan Orozco <ivan.oroz34@gmail.com>
2023-09-03 19:06:55 -0500

    First upload

---
+++ Introduction.m
@@ -0,0 +1,27 @@
+% Important comands:
+
+% whos -> Shows a table, with the variables information.
+% clear <variable> -> Delete the variable.
+% clear all -> Delete all the variables.
+% [up arrow] -> Passing above the course above the command line, and then 
+%               Push the up arrow, allows us to see the command history.
+
+% Matrix syntaxis:
+% a = [ 1 2 3 ; 4 5 6 ; 7 8 9 ] -> the values are separated using spaces,
+% and the rows are separated with the ';' caracter.
+
+% String syntaxis:
+% string = "Hello this is my first string!"
+% a = string("Hello i am string type!") -> This is function to specify the
+% type of the varible is a string.
+% string_matrix = 
+%               [   string("I am the first sentence!") 
+%                   string("I am the second sentence!") ;
+%                   string("I am the third sentence!") 
+%                   string("I am the fourth sentence!") 
+%               ]
+
+% Logical function:
+% logical_matrix = logical( [ 1 0 1 ; 1 1 1 ] ) -> Allows to build a matrix
+% that allows only '1' or '0' values ( if we put a value diferente of  1 
+% or 0, it replaces with a '1' value ).
---
+++ Scripts_and_semicolon.m
@@ -0,0 +1,2 @@
+% Fast way to save commands into a script:
+% Select all commands of interes to safe, then we have the 
